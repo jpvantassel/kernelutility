2c5f8bd23e67a9ffb3ecfa0e1b0ce737fc1c4457
Joseph Vantassel <jvantassel@utexas.edu>
2022-02-28 15:37:24 -0600

    :bug: Fix typo in setup.py

--- setup.py
+++ setup.py
@@ -33,10 +33,10 @@ setup(
 
         "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
 
-        "Programming Language :: Python :: 3.7"
-        "Programming Language :: Python :: 3.8"
-        "Programming Language :: Python :: 3.9"
-        "Programming Language :: Python :: 3.10"
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
     ],
     keywords='jupyter kernel conda',
     packages=find_packages(),
